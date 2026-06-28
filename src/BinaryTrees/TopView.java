6b14c4db0f03828093bd46d521d9812dd64b5349
Sankalp Tiwari <sankalptiwari94255@gmail.com>
2026-06-28 22:48:27 +0530

    Binary Tree : TopView

---
+++ src/BinaryTrees/TopView.java
@@ -0,0 +1,92 @@
+package BinaryTrees;
+
+import java.util.*;
+
+public class TopView {
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+            left = right = null;
+        }
+    }
+
+    static class Pair {
+        Node node;
+        int hd;
+
+        Pair(Node node, int hd) {
+            this.node = node;
+            this.hd = hd;
+        }
+    }
+
+    public static void topView(Node root) {
+
+        if (root == null)
+            return;
+
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        Queue<Pair> queue = new LinkedList<>();
+
+        queue.offer(new Pair(root, 0));
+
+        while (!queue.isEmpty()) {
+
+            Pair current = queue.poll();
+            Node node = current.node;
+            int hd = current.hd;
+
+            // Store only the first node at each horizontal distance
+            if (!map.containsKey(hd)) {
+                map.put(hd, node.data);
+            }
+
+            if (node.left != null) {
+                queue.offer(new Pair(node.left, hd - 1));
+            }
+
+            if (node.right != null) {
+                queue.offer(new Pair(node.right, hd + 1));
+            }
+        }
+
+        System.out.print("Top View: ");
+
+        for (int value : map.values()) {
+            System.out.print(value + " ");
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        /*
+                    1
+                  /   \
+                 2     3
+                / \   / \
+               4   5 6   7
+                    \
+                     8
+        */
+
+        Node root = new Node(1);
+
+        root.left = new Node(2);
+        root.right = new Node(3);
+
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        root.left.right.right = new Node(8);
+
+        topView(root);
+    }
+}
